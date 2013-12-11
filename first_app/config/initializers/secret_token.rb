8d2cf1f7cd797416c768182c0210321739a0ea8c
Lucia Rodriguez <locia@nhecos.com>
2013-12-11 03:06:17 +0000

    Lets get pro

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8e5da2588c0782770a1de914a0b82c1bc3e3853df8d1b0dff4cf814fa132eb111eecc13697329f9c8e8087a4f7af178ccb1c36ad97fa2f352e1eb18706621c56'
---
+++ letsgetpro/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Letsgetpro::Application.config.session_store :cookie_store, key: '_letsgetpro_session'
